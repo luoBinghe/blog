e2ca5b3b5bbaed4b23c9902b8e9cce336b9ca36e
guayana <isabelaguayanaz@gmail.com>
2021-08-16 19:16:35 -0300

    estilização dos posts

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -3,6 +3,7 @@ import { GetStaticPaths, GetStaticProps } from 'next';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
+import { FaCalendarAlt, FaUser } from 'react-icons/fa'
 import Header from '../../components/Header';
 import styles from './post.module.scss';
 
@@ -29,7 +30,43 @@ interface PostProps {
 
 export default function Post() {
   return(
+    <>
     <Header />
+    <div className={styles.container}>
+      <div className={styles.banner}>
+        <img src="/images/banner.jpeg" />
+      </div>
+        <main>
+          <section>
+            <h1>Criando um app CRA do zero</h1>
+            <div className={styles.span}>
+            <span>
+              <FaCalendarAlt className={styles.icon} />
+              15 Mar 2021 
+            </span>
+            <span>
+              <FaUser className={styles.icon}/>
+              Joseph Oliveira
+            </span>
+          </div>
+          </section>
+          <article>
+            <h3>Proin et varius</h3>
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.
+            </p>
+            <br/>
+            <br/>
+            <p>
+            t has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+            </p>
+          </article>
+        </main>
+      </div>
+    </>
   )
 }
 
