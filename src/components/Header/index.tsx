61ffcfb73398dd263e5360239b97fa30a6856829
guayana <isabelaguayanaz@gmail.com>
2021-08-16 18:39:54 -0300

    redirecionamento para a página inicial

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
 import style from './header.module.scss'
+import Link from 'next/link'
 
 export default function () {
   return(
     <div className={style.container}>
       <header>
-        <img src="/images/logo.svg"/>
+        <Link href="/">
+          <a>
+            <img src="/images/logo.svg"/>
+          </a>
+        </Link>
       </header>
     </div>
   )
